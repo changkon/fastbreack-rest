ce468ae1b863288f6ba625b75529b85e8d73dc89
Changkon Han <chang_kon@hotmail.com>
2021-02-12 15:12:08 +0900

    Initial commit

---
+++ src/test/java/com/changkon/fastbreackrest/FastbreackRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changkon.fastbreackrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastbreackRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
